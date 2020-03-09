d7a610e8eb868dd64d7ad2a1c0058c6085dc8421
hjweinreb <60324170+hjweinreb@users.noreply.github.com>
2020-03-08 22:42:28 -0400

    I did it!

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,19 @@ import Footer from './Footer';
 import './App.css';
 
 function App(props) {
+  console.log(props);
   return (
     <div className="wrapper">
+
       <Header />
-      <ChatStream />
+      <ChatStream
+        messages={props.conversation.messages}
+        currentUser={props.currentUser}
+      />
       <Footer />
     </div>
   );
 }
 
 export default App;
+
